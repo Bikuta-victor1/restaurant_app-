633c2c1abe8dde376d2be4da49f54ea8dcb62045
Bikuta-Victor1 <adekolavictor51@gmail.com>
2021-01-04 11:16:15 +0100

    Code Refactoring

--- lib/widgets/badge.dart
+++ lib/widgets/badge.dart
@@ -47,31 +47,34 @@ class _IconBadgeState extends State<IconBadge> {
           widget.icon,
           size: widget.size,
         ),
-        Positioned(
-          right: 0.0,
-          child: Container(
-            padding: EdgeInsets.all(1),
-            decoration: BoxDecoration(
-              color: Colors.red,
-              borderRadius: BorderRadius.circular(6),
-            ),
-            constraints: BoxConstraints(
-              minWidth: 13,
-              minHeight: 13,
-            ),
-            child: Padding(
-              padding: EdgeInsets.only(top: 1),
-              child: Text(
-                mynewCartlength == null ? "0" : "${mynewCartlength}",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 8,
+        mynewCartlength != 0
+            ? Positioned(
+                right: 0.0,
+                child: Container(
+                  padding: EdgeInsets.all(1),
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                    borderRadius: BorderRadius.circular(6),
+                  ),
+                  constraints: BoxConstraints(
+                    minWidth: 13,
+                    minHeight: 13,
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.only(top: 1),
+                    child: Text(
+                      //mynewCartlength == null ? "0" :
+                      "${mynewCartlength}",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 8,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
                 ),
-                textAlign: TextAlign.center,
-              ),
-            ),
-          ),
-        ),
+              )
+            : Container(),
       ],
     );
   }
